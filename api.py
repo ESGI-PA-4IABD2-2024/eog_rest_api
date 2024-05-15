284d25678c2e03ba1c3017c9037209fd1f862135
AntoineTSIO <antoine.touzart@outlook.com>
2024-05-15 23:02:41 +0200

    feat: start creating api

---
+++ api.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
+app = FastAPI()
+
+origins = [
+    "http://localhost"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/departure-arrival/{departure_location}/{arrival_location}")
+def departure_arrival(departure_location: str, arrival_location: str):
+    print(f'Departure: {departure_location}')
+    print(f'Arrival: {arrival_location}')
+    return {
+        "Departure": departure_location,
+        "Arrival": arrival_location
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
