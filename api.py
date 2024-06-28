c4ea3cb14d121ca6557c0d40435f1a9e3863edb5
AntoineTSIO <antoine.touzart@outlook.com>
2024-06-28 23:46:54 +0200

    feat: add tmp node and changement list to api response

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi import Response
 from fastapi.middleware.cors import CORSMiddleware
 
 from db_interactions import get_open_stations
@@ -24,10 +25,36 @@ def request_stations():
 
 
 @app.get("/departure-arrival/{departure_location}/{arrival_location}")
-def departure_arrival(departure_location: str, arrival_location: str):
+def departure_arrival(departure_location: str, arrival_location: str, response: Response):
     print(f"Departure: {departure_location}")
     print(f"Arrival: {arrival_location}")
-    return {"Departure": departure_location, "Arrival": arrival_location}
+    departure_to_arrival = {
+        "departure": departure_location,
+        "node_1": "node_1",
+        "node_2": "node_2",
+        "node_3": "node_3",
+        "node_4": "node_4",
+        "node_5": "node_5",
+        "node_6": "node_6",
+        "node_7": "node_7",
+        "changement_1": "node_8",
+        "changement_2": "node_9",
+        "node_10": "node_10",
+        "node_11": "node_11",
+        "node_12": "node_12",
+        "node_13": "node_13",
+        "node_14": "node_14",
+        "node_15": "node_15",
+        "node_16": "node_16",
+        "changement_3": "node_17",
+        "changement_4": "node_18",
+        "node_19": "node_19",
+        "node_20": "node_20",
+        "node_21": "node_21",
+        "arrival": arrival_location,
+    }
+    response.headers["Content-Type"] = "application/json; charset=utf-8"
+    return departure_to_arrival
 
 
 if __name__ == "__main__":
